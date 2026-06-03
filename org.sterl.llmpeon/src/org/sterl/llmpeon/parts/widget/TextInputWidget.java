3d6c525613e3865b0a6281237d7af914ffccfc83
Istvan Petres <pijulius@users.noreply.github.com>
2026-06-03 20:27:30 +0300

    Add undo/redo and context menu support for user input field. (#72)
    * Enable sendThinking option in AiProvider configuration
    
    This should fix it to work with DeepSeek and prevent the error:
    The `reasoning_content` in the thinking mode must be passed back to the API.
    
    * Added possibility to require confirmation for shell commands before run.
    
    This adds a new settings option called 'Require confirmation for shell commands before run' and if checked it will require you to confirm each and every shell command that is being run. It also allows you to change the command by entering a new command instead. It is turned off by default and uses the newly added UserQuestionWidget.
    
    * Multiple shell command confirmation options
    
    Added possibility to set shell command confirmation to 'Never Required', 'Always Required' or 'Except when Autonomous'
    
    * Added true false checks with false as default for Shell Confirmation Settings.
    
    * Fixed falling back to getLocation when getRawLocation returns null.
    
    * Fixed Send thinking back to LLM preference was never read so it was always false.
    
    * Fixed another getRawLocation being null.
    
    * Delete dummy orig file
    
    * Add Undo/Redo and Context Menu support for user input text field.
    
    Adds CTRL+Z and CTRL+SHIFT+Z support for undo and redo functionality for
    the TextInputWidget, also adds a context menu that includes:
    Undo
    Redo
    Cut
    Copy
    Paste
    Select All
    
    All Credits go to "Apache Hop", it's a crude portover of their
    implementation.
    
    ---------
    
    Co-authored-by: pijulius <567420+pijulius@users.noreply.github.com>

--- org.sterl.llmpeon/src/org/sterl/llmpeon/parts/widget/TextInputWidget.java
+++ org.sterl.llmpeon/src/org/sterl/llmpeon/parts/widget/TextInputWidget.java
@@ -1,5 +1,8 @@
 package org.sterl.llmpeon.parts.widget;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.custom.VerifyKeyListener;
@@ -10,6 +13,8 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 
 /**
  * Reusable auto-growing StyledText widget. The text area grows from a minimum of
@@ -22,10 +27,20 @@ public class TextInputWidget extends Composite {
     private final int maxRows;
     private final Runnable onReflow;
 
+    private static final int MAX_STACK_SIZE = 25;
+    private List<UndoRedoStack> undoStack;
+    private List<UndoRedoStack> redoStack;
+
+    private final Menu popupMenu;
+    private boolean fullSelection = false;
+
     public TextInputWidget(Composite parent, int style, int maxRows, Runnable onReflow) {
         super(parent, style);
         this.maxRows = maxRows;
         this.onReflow = onReflow;
+
+        undoStack = new LinkedList<>();
+        redoStack = new LinkedList<>();
 
         GridLayout layout = new GridLayout(1, false);
         layout.marginWidth = 0;
@@ -35,6 +50,9 @@ public class TextInputWidget extends Composite {
         styledText = new StyledText(this, SWT.MULTI | SWT.WRAP);
         styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
         styledText.addModifyListener(e -> refreshHeight());
+
+        popupMenu = new Menu(parent.getShell(), SWT.POP_UP);
+        addUndoRedoSupport(popupMenu);
     }
 
     private void refreshHeight() {
@@ -50,6 +68,156 @@ public class TextInputWidget extends Composite {
         if (gd.heightHint != newHint) {
             gd.heightHint = newHint;
             onReflow.run();
+        }
+    }
+
+    // Add support functions for Undo/Redo with Popup Menu on text widget
+    protected void addUndoRedoSupport(Menu popupMenu) {
+        final MenuItem undoItem = new MenuItem(popupMenu, SWT.PUSH);
+        undoItem.setText("Undo");
+        undoItem.addListener(SWT.Selection, event -> undo());
+
+        final MenuItem redoItem = new MenuItem(popupMenu, SWT.PUSH);
+        redoItem.setText("Redo");
+        redoItem.addListener(SWT.Selection, event -> redo());
+
+        new MenuItem(popupMenu, SWT.SEPARATOR);
+
+        final MenuItem cutItem = new MenuItem(popupMenu, SWT.PUSH);
+        cutItem.setText("Cut");
+        cutItem.addListener(SWT.Selection, event -> styledText.cut());
+
+        final MenuItem copyItem = new MenuItem(popupMenu, SWT.PUSH);
+        copyItem.setText("Copy");
+        copyItem.addListener(SWT.Selection, event -> styledText.copy());
+
+        final MenuItem pasteItem = new MenuItem(popupMenu, SWT.PUSH);
+        pasteItem.setText("Paste");
+        pasteItem.addListener(SWT.Selection, event -> styledText.paste());
+
+        new MenuItem(popupMenu, SWT.SEPARATOR);
+
+        final MenuItem selectAllItem = new MenuItem(popupMenu, SWT.PUSH);
+        selectAllItem.setText("Select All");
+        selectAllItem.addListener(SWT.Selection, event -> styledText.selectAll());
+
+        styledText.setMenu(popupMenu);
+
+        styledText.addListener(
+            SWT.Selection,
+            event -> {
+                if (styledText.getSelectionCount() == styledText.getCharCount()) {
+                    fullSelection = true;
+                }
+            });
+
+		styledText.addListener(
+            SWT.KeyDown,
+            event -> {
+                if (event.keyCode == 'z'
+                    && (event.stateMask & SWT.MOD1) != 0
+                    && (event.stateMask & SWT.MOD2) != 0) {
+                    redo();
+                } else if (event.keyCode == 'z'
+                    && (event.stateMask & SWT.MOD1) != 0) {
+                    undo();
+                } else if (event.keyCode == 'a' && (event.stateMask & SWT.MOD1) != 0) {
+                    styledText.selectAll();
+                }
+            });
+
+        styledText.addExtendedModifyListener(
+            event -> {
+                int eventLength = event.length;
+                int eventStartPostition = event.start;
+
+                String newText = getText();
+                String repText = event.replacedText;
+                String oldText = "";
+                int eventType = -1;
+
+                if ((event.length != newText.length()) || (fullSelection)) {
+                    if (repText != null && !repText.isEmpty()) {
+                        oldText =
+                              newText.substring(0, event.start)
+                              + repText
+                              + newText.substring(event.start + event.length);
+                        eventType = UndoRedoStack.DELETE;
+                        eventLength = repText.length();
+                    } else {
+                        oldText =
+                              newText.substring(0, event.start) + newText.substring(event.start + event.length);
+                        eventType = UndoRedoStack.INSERT;
+                    }
+
+                    if ((oldText != null && !oldText.isEmpty()) || (eventStartPostition == event.length)) {
+                        UndoRedoStack urs =
+                            new UndoRedoStack(eventStartPostition, newText, oldText, eventLength, eventType);
+
+                        // Stack is full
+                        if (undoStack.size() == MAX_STACK_SIZE) {
+                            undoStack.remove(undoStack.size() - 1);
+                        }
+                        undoStack.add(0, urs);
+                    }
+                }
+                fullSelection = false;
+            });
+    }
+
+    protected void undo() {
+        if (!undoStack.isEmpty()) {
+            UndoRedoStack undo = undoStack.remove(0);
+            if (redoStack.size() == MAX_STACK_SIZE) {
+                redoStack.remove(redoStack.size() - 1);
+            }
+            UndoRedoStack redo =
+                new UndoRedoStack(
+                    undo.getCursorPosition(),
+                    undo.getReplacedText(),
+                    getText(),
+                    undo.getEventLength(),
+                    undo.getType());
+            fullSelection = false;
+            setText(undo.getReplacedText());
+            if (undo.getType() == UndoRedoStack.INSERT) {
+                styledText.setCaretOffset(undo.getCursorPosition());
+            } else if (undo.getType() == UndoRedoStack.DELETE) {
+                styledText.setCaretOffset(undo.getCursorPosition() + undo.getEventLength());
+                styledText.setSelection(undo.getCursorPosition(), undo.getCursorPosition() + undo.getEventLength());
+                if (styledText.getSelectionCount() == styledText.getCharCount()) {
+                    fullSelection = true;
+                }
+            }
+            redoStack.add(0, redo);
+        }
+    }
+
+    protected void redo() {
+        if (!redoStack.isEmpty()) {
+            UndoRedoStack redo = redoStack.remove(0);
+            if (undoStack.size() == MAX_STACK_SIZE) {
+                undoStack.remove(undoStack.size() - 1);
+            }
+            UndoRedoStack undo =
+                new UndoRedoStack(
+                    redo.getCursorPosition(),
+                    redo.getReplacedText(),
+                    getText(),
+                    redo.getEventLength(),
+                    redo.getType());
+            fullSelection = false;
+            setText(redo.getReplacedText());
+            if (redo.getType() == UndoRedoStack.INSERT) {
+                styledText.setCaretOffset(redo.getCursorPosition());
+            } else if (redo.getType() == UndoRedoStack.DELETE) {
+                styledText.setCaretOffset(redo.getCursorPosition() + redo.getEventLength());
+                styledText.setSelection(redo.getCursorPosition(), redo.getCursorPosition() + redo.getEventLength());
+                if (styledText.getSelectionCount() == styledText.getCharCount()) {
+                    fullSelection = true;
+                }
+            }
+            undoStack.add(0, undo);
         }
     }
 
@@ -106,5 +274,45 @@ public class TextInputWidget extends Composite {
         if (styledText.isDisposed()) return null;
         var local = styledText.getLocationAtOffset(styledText.getCaretOffset());
         return styledText.toDisplay(local.x, local.y);
+    }
+
+    public class UndoRedoStack {
+        public static final int DELETE = 0;
+        public static final int INSERT = 1;
+
+        private String strNewText;
+        private String strReplacedText;
+        private int iCursorPosition;
+        private int iEventLength;
+        private int iType;
+
+        public UndoRedoStack(
+            int iCursorPosition, String strNewText, String strReplacedText, int iEventLength, int iType) {
+            this.iCursorPosition = iCursorPosition;
+            this.strNewText = strNewText;
+            this.strReplacedText = strReplacedText;
+            this.iEventLength = iEventLength;
+            this.iType = iType;
+        }
+
+        public String getReplacedText() {
+          return this.strReplacedText;
+        }
+
+        public String getNewText() {
+          return this.strNewText;
+        }
+
+        public int getCursorPosition() {
+          return this.iCursorPosition;
+        }
+
+        public int getEventLength() {
+          return iEventLength;
+        }
+
+        public int getType() {
+          return iType;
+        }
     }
 }
